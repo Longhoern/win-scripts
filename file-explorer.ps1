dea4455ce77eeca5880225877ebd9f8852a781e8
Longhorn <76806137+Longhoern@users.noreply.github.com>
2024-10-01 10:32:33 -0500

    Update file-explorer.ps1

--- file-explorer.ps1
+++ file-explorer.ps1
@@ -1,4 +1,4 @@
-# PowerShell script to show hidden files and file extensions in Windows Explorer
+# PowerShell script to configure file explorer
 
 # Show hidden files
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 1
@@ -6,8 +6,12 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 # Show file extensions
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
 
+# Create the required registry key
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force | Out-Null
+
+# Set the default value to an empty string
+Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value "" -Force
+
 # Restart Windows Explorer to apply changes
 Stop-Process -Name "explorer" -Force
 Start-Process "explorer"
-
-Write-Host "Hidden files and file extensions are now visible in Windows Explorer."
